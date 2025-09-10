b087d85c076e48a165062298962937791b3df3e4
Akhila Thallapelly <akhilathallapelly9@gmail.com>
2025-09-10 21:20:03 +0530

    updated commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,23 @@
 import Restaurantcard from "./Restaurantcard";
 import reslist from "../utils/mockdata";
+
+import {useState} from "react";
 const Body=()=>{
+    const [lstreslist,setlstresList]=useState(reslist);
     return(
+        
+        
         <div className="body">
             <div className="search">
-                search
+                <button onClick={()=>{
+                    console.log("clicked");
+                    let filtered=lstreslist.filter(res=>res.info.avgRating>=4.5);
+                    console.log(filtered);
+                    setlstresList(filtered);
+                }}>Top restaurants</button>
             </div>
             <div className="res-con">
-                {reslist.map((li)=>{
+                {lstreslist.map((li)=>{
                     return <Restaurantcard key={li.info.id} resdata={li.info}/>
                 })}
                 {/* <Restaurantcard resdata={reslist[0]?.info}/> */}
